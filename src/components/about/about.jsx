f45cf34529f1bd25722224374dc002b84f329e9f
Miguel R <contact@migue.pro>
2021-11-06 20:52:45 -0400

    clean

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -10,6 +10,12 @@ const About = () => {
                 </div>
                 <div className="stripe"> </div>
             </div>
+            <div className="about-container">
+                <h1>Hi there!</h1>
+                <h2>I'm Miguel</h2>
+                <h3>and I make Web Applications</h3>
+                <p>Software Developer with with a solid background as military officer.</p>
+            </div>
         </div>
     )
 }
