d29c73b5726f5cc9cf50becae7e3c90288231101
Miguel R <contact@migue.pro>
2021-11-09 14:10:34 -0500

    pills in the background of the projects

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
+import styled from 'styled-components'
 import "./projects.scss"
 
 const Projects = () => {
+    const [slideIndex, setSlideIndex] = useState(0)
     return (
         <div id="Projects"  className='section'>
-            <div className="section-name">
-                <div>
-                    Projects
-                </div>
-                <div className="stripe"> </div>
+            <div className="bg-shape"/>
+            <div className="bg-shape back"/>
+            <div className="section-info">
+                <h1>Title of the project</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis debitis enim possimus saepe. A at commodi cumque dolores earum enim eum hic minus natus repellendus! Debitis eos maiores quis ratione!</p>
             </div>
-            <div> </div>
+            <div className="section-img">
+            </div>
         </div>
     )
 }
