79ce81c2e506efe1001712c959b0b7182668657d
Miguel R <contact@migue.pro>
2021-11-10 13:36:49 -0500

    slider working properly

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -3,21 +3,22 @@ import styled from "styled-components";
 import "./projects.scss";
 
 const Image = styled.div`
-    // background: url("https://www.migue.pro/static/images/webprog/network.gif") no-repeat;
     background: url(${props => props.img}) no-repeat;
-`;
-
+    // margin-left: calc(${props => props.id} * 100vw);
+    `;
 const Projects = ({item}) => {
 	return (
 		<div id="Projects" className="section">
 			<div className="bg-shape"/>
 			<div className="bg-shape back"/>
 			<div className="wrapper">
+
 				<div className="section-info">
 					<h1>{item.title}</h1>
 					<p>{item.desc}</p>
 				</div>
-				<Image className="section-img" img={item.img}>
+
+				<Image className="section-img" img={item.img} id={item.id}>
 				</Image>
 			</div>
 		</div>
