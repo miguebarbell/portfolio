ffbf181cfc41a248e5ababd2fc3b01fcbf83d8d0
Miguel R <contact@migue.pro>
2021-11-10 02:04:05 -0500

    stripes background

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -3,18 +3,29 @@ import styled from 'styled-components'
 import "./projects.scss"
 
 const Projects = () => {
-    const [slideIndex, setSlideIndex] = useState(0)
+    const [
+        slideIndex,
+        setSlideIndex
+    ] = useState(0)
+
+
     return (
-        <div id="Projects"  className='section'>
+        <div id="Projects" className="section">
+            <div className="section-name">
+                <div>
+                    About
+                </div>
+                <div className="stripe"> </div>
+            </div>
             <div className="bg-shape"/>
             <div className="bg-shape back"/>
             <div className="wrapper">
-            <div className="section-info">
-                <h1>Network - Social Media</h1>
-                <p>This is a project that imitate the main functionality of twitter.</p>
-            </div>
-            <div className="section-img">
-            </div>
+                <div className="section-info">
+                    <h1>Network - Social Media</h1>
+                    <p>This is a project that imitate the main functionality of twitter.</p>
+                </div>
+                <div className="section-img">
+                </div>
             </div>
         </div>
     )
