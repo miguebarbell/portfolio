8e2fda6814da8ea8f837fa1cf24eca3748a0006a
Miguel R <contact@migue.pro>
2021-11-10 14:26:09 -0500

    added others projects to the data

--- src/data.js
+++ src/data.js
@@ -3,12 +3,56 @@ export const ProjectsData = [
 		"id": 1,
 		"title": "Network",
 		"desc": "Twitter like social network website for making posts and following users. makes API calls to gather the posts.",
-		"img": "https://www.migue.pro/static/images/webprog/network.gif"
+		"img": "https://www.migue.pro/static/images/webprog/network.gif",
+		"link": "https://miguepro.herokuapp.com/network"
 	},
 	{
 		"id": 2,
 		"title": "Mail",
 		"desc": "Simple email client, can send, receive, archive and reply the messages. Make API calls for send and receive emails.",
-		"img": "https://www.migue.pro/static/images/webprog/mail.gif"
+		"img": "https://www.migue.pro/static/images/webprog/mail.gif",
+		"link": "https://miguepro.herokuapp.com/mail"
+	},
+	{
+		"id": 3,
+		"title": "e-commerce",
+		"desc": "e-Bay like ecommerce auction site that will allow users to post auctions listings, place bids on listings, comment on those listings and add listing to a watchlist.",
+		"img": "https://www.migue.pro/static/images/webprog/ecomerce.gif",
+		"link": "https://miguepro.herokuapp.com/ecommerce/"
+	},
+	{
+		"id": 4,
+		"title": "safe-app",
+		"desc": "Password manager, save, edit, generate strong passwords, export database, delete user, that have the capability of generate random and strong passwords, the length and symbols are set from the user.",
+		"img": "https://www.migue.pro/static/images/webprog/passwords.gif",
+		"link": "https://miguepro.herokuapp.com/safeapp"
+	},
+	{
+		"id": 5,
+		"title": "Sympathy score",
+		"desc": "Predict a percentage of the smile detected in every face in a picture.",
+		"img": "https://www.migue.pro/static/images/deeplearning/gifs.gif",
+		"link": "https://github.com/miguebarbell/sympathy"
+	},
+	{
+		"id": 6,
+		"title": "FaceRecognition",
+		"desc": "Clean pipeline where you provide pictures and recognize it in other pictures,",
+		"img": "https://www.migue.pro/static/images/deeplearning/giff.gif",
+		"link": "https://github.com/miguebarbell/face_detection"
+	},
+	{
+		"id": 7,
+		"title": "Anonymizer",
+		"desc": "Put a desired filter in every face that you select in a picture.",
+		"img": "https://www.migue.pro/static/images/deeplearning/gifa.gif",
+		"link": "https://github.com/miguebarbell/anonymizer"
+	},
+	{
+		"id": 8,
+		"title": "HeightEstimator",
+		"desc": "With the help of a aruco tag, can estimate the height of someone.",
+		"img": "https://www.migue.pro/static/images/deeplearning/gifh.gif",
+		"link": "https://github.com/miguebarbell/heightestimator"
 	},
 ];
