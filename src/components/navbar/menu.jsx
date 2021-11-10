c20f2c416de1e49fb8d256e808c6fe0c9ee9fd2b
Miguel R <contact@migue.pro>
2021-11-10 03:30:44 -0500

    fixed zindex with the new adds

--- src/components/navbar/menu.jsx
+++ src/components/navbar/menu.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import "./menu.scss"
+import React from "react";
+import "./menu.scss";
 
 const Menu = ({menuOpen}) => {
-    return (
-        <div id="Menu" className={(menuOpen && "active")}>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Projects</li>
-                <li>Contact</li>
-            </ul>
-        </div>
-    )
-}
+	return (
+		<div id="Menu" className={(menuOpen && "active")}>
+			<ul>
+				<li>Home</li>
+				<li>About</li>
+				<li>Projects</li>
+				<li>Contact</li>
+			</ul>
+		</div>
+	);
+};
 
-export default Menu
+export default Menu;
