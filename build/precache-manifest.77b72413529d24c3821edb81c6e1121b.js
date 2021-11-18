a8e6c3db25116745bc12510a7294b6306f55ecf5
Miguel R <contact@migue.pro>
2021-11-18 11:56:52 -0500

    trying

---
+++ build/precache-manifest.77b72413529d24c3821edb81c6e1121b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51c71e14a3934dd8c3ba24f14f974fb4",
+    "url": "/miguebarbell.github.io/portfolio/index.html"
+  },
+  {
+    "revision": "9323f6d658963cd82267",
+    "url": "/miguebarbell.github.io/portfolio/static/css/main.a4dcc9cc.chunk.css"
+  },
+  {
+    "revision": "8786c63e4c7ca33a9578",
+    "url": "/miguebarbell.github.io/portfolio/static/js/2.edd36d2b.chunk.js"
+  },
+  {
+    "revision": "ea88211925d18270e09b4691b32a2900",
+    "url": "/miguebarbell.github.io/portfolio/static/js/2.edd36d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9323f6d658963cd82267",
+    "url": "/miguebarbell.github.io/portfolio/static/js/main.ef85840b.chunk.js"
+  },
+  {
+    "revision": "0806f5d6599322ba1129",
+    "url": "/miguebarbell.github.io/portfolio/static/js/runtime-main.c3226bd6.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.92a29ddfffaecf012246dcd9b3a56ff6.js"
+  "/miguebarbell.github.io/portfolio/precache-manifest.77b72413529d24c3821edb81c6e1121b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/miguebarbell.github.io/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
